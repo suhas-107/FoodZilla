b5d98a84d464a17931f294035563cdab66a083ee
Suhas Deshpande <deshpandesuhas107@gmail.com>
2024-01-02 22:37:32 +0530

    Your commit message here

---
+++ Components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div className="footer">
+        Created By
+        <i className="fa-solid fa-heart"></i>
+        <a href="https://www.linkedin.com/in/suhas-deshpande-14a52125b/" target="_blank">
+          Suhas Deshpande
+        </a>
+        <i className="fa-solid fa-copyright"></i>2023
+        <strong>
+          Food<span>Fire</span>
+        </strong>
+      </div>
+    );
+  };
+  
+  export default Footer;
---
+++ constants.js
@@ -0,0 +1,8 @@
+export const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+// Swiggy API to get Restaurant data with corsproxy
+export const swiggy_api_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.642762&lng=73.81396889999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const shimmer_card_unit = 20;
